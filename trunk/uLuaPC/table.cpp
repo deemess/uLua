500c55a4b7acf148af9246cad9e7df6d8a83471e
anokhindm@gmail.com <anokhindm@gmail.com@ee0af25c-ad45-2cb5-69e3-7aacd644f6d8>
2013-02-27 14:41:55 +0000

    Starting table implementation.

--- trunk/uLuaPC/table.cpp
+++ trunk/uLuaPC/table.cpp
@@ -6,12 +6,59 @@ gcvarpt* tableNew()
 	return gcNew(VAR_TABLE, DEFAULT_TABLE_SIZE);
 }
 
-//put value into table for a key
-void tablePut(vmregister* key, vmregister* val)
+u32 getHash(vmregister* val)
 {
+	switch(val->type)
+	{
+
+	case VAR_BOOLEAN:
+		return val->numval;
+
+	case VAR_NUMBER:
+	case VAR_FLOAT:
+		return val->numval;
+
+	case VAR_STRING:
+		//TODO: implement hashing
+		return 0;
+
+	case VAR_NULL:
+		return 0;
+
+	case VAR_FILE_POINTER:
+		return val->numval;
+
+	case VAR_TABLE:
+		//TODO: implement hashing
+		return 0;
+
+	}
+	return 0;
+}
+
+//put value into table for a key
+void tablePut(gcvarpt* table, vmregister* key, vmregister* val)
+{
+	//get table structure
+	hashtable* tb = (hashtable*)(*table)->data;
+
+	//compute hash for the key and index
+	u32 hashkey = getHash(key);
+	u08 keyindex = hashkey % tb->size;
+
+	//find empty slot
+	//TODO: process running out from size
+	for(u08 i=keyindex; i < tb->size; i++)
+	{
+		if(tb->keys[i] = NULL)
+		{
+			keyindex = i; 
+			break;
+		}
+	}
 }
 
 //get value from table for key and save it into val
-void tableGet(vmregister* key, vmregister* val)
+void tableGet(gcvarpt* table, vmregister* key, vmregister* val)
 {
 }
