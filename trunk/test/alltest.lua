0791fdad8ef11e5e7a8f84c7b2fa289f0fed4f63
anokhindm@gmail.com <anokhindm@gmail.com@ee0af25c-ad45-2cb5-69e3-7aacd644f6d8>
2013-12-05 12:28:04 +0000

    Added build to the test directory. Added test/alltest.bat to test all implemented features.

---
+++ trunk/test/alltest.lua
@@ -0,0 +1,27 @@
+-- for test
+for i = 1, 10, 1 do
+	print(i)
+end 
+
+-- func test
+function func1(par1,par2)
+	local local1 = par1 + par2
+	return local1
+end
+
+function func2(par1,par2,par3)
+	local local1 = par1 + par2
+	local local2 = par3 * par3
+	local1 = local1 + local2
+	return local1
+end
+
+a=1
+b=3
+c=3.5
+
+result1 = func1(a,b)
+result2 = func2(a,b,c)
+
+print("Result1 = ", result1)
+print("Result2 = ", result2)
