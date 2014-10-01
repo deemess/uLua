a0c39066fa2e2f4d2856a0a1770d4a0d0dd53aef
Dmitry <anokhindm@outlook.com>
2014-10-01 18:51:49 +0400

    Added constant checking for unique.

--- ucodegen.c
+++ ucodegen.c
@@ -1,8 +1,6 @@
 #include "ucodegen.h"
 #include <stdio.h>
 
-
-
 void initFunction(Function* f, u08* code) {
 	u08 i;
 
@@ -19,7 +17,26 @@ void initFunction(Function* f, u08* code) {
 	}
 }
 
-void pushConst(Function* f, Constant* c) {
+BOOL constEqueals(Function* f, Constant* a, Constant* b) {
+	u08 i;
+	if(a->type == b->type) {
+		if(a->type == CG_CONST_NUMBER) {
+			if(a->number == b->number)
+				return TRUE;
+		} else if(a->type == CG_CONST_STRING) {
+			if(a->string.bplen == b->string.bplen){
+				for(i=0; i<a->string.bplen; i++) {
+					if(f->code[a->string.bp + i] != f->code[b->string.bp + i]) 
+						return FALSE;
+				}
+				return TRUE;
+			}
+		}
+	}
+	return FALSE;
+}
+
+Constant* pushConst(Function* f, Constant* c) {
 	Constant* last;
 
 	if(f->c == NULL) {
@@ -29,12 +46,20 @@ void pushConst(Function* f, Constant* c) {
 	}
 
 	last = f->c;
+	if(constEqueals(f, last, c)) {
+		free(c);
+		return last;
+	}
 	while(last->next != NULL) {
 		last = last->next;
+		if(constEqueals(f, last, c)) {
+			free(c);
+			return last;
+		}
 	}
 
-	c->n = last->n + 1;
 	last->next = c;
+	c->n = last->n + 1;
 }
 
 Constant* pushConstString(Function* f, SString* str) {
@@ -47,7 +72,7 @@ Constant* pushConstString(Function* f, SString* str) {
 	c->string.bp = str->bp;
 	c->string.bplen = str->bplen;
 	
-	pushConst(f, c);
+	c = pushConst(f, c);
 
 	return c;
 }
@@ -60,7 +85,7 @@ Constant* pushConstNumber(Function* f, float number) {
 	c->type = CG_CONST_NUMBER;
 	c->number = number;
 	
-	pushConst(f, c);
+	c = pushConst(f, c);
 
 	return c;
 }
