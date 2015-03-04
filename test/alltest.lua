114dae3484d1fff68faec89026356cc3d78082c1
Dmitry <anokhindm@gmail.com>
2015-03-04 13:21:47 +0300

    Updated alltest.lua with currently supported features

--- test/alltest.lua
+++ test/alltest.lua
@@ -1,77 +1,26 @@
---load nil test
-function loadtest()
-	local a,b,c,d,e = nil,nil,0
-	local aa,bb = true,false
-	print(a,b,c)
-	print(aa,bb)
-end
-loadtest()
+a = 15
+b = (5+2)/2 + a
+a = b/2
 
--- for loop test
-for i = 1, 3, 1 do
-	print(i)
-end 
+print('a=')
+print(a)
+print('b=')
+print(b)
 
-for i = 3, 1, -1 do
-	print(i)
-end 
-
--- upvalue test
-function newCounter ()
-	local i = 0
-	return function ()   -- anonymous function
-			i = i + 1
-			return i
-		end
-end
-c1 = newCounter()
-print(c1())  --> 1
-print(c1())  --> 2
-print(c1())  --> 3
-
-
--- function call test
-function func1(par1,par2)
-	local local1 = par1 + par2
-	return local1
+if a > 20 then
+	print('a > 20')
+elseif a < 10 then
+	print('a < 10')
+elseif a == 10 then
+	print('a = 10')
+elseif a == 20 then
+	print('a = 20')
+else
+	print('a > 10 and a < 20')
 end
 
-function func2(par1,par2,par3)
-	local local1 = par1 + par2
-	local local2 = par3 * par3
-	local1 = local1 + local2
-	return local1
-end
-
-a=1
-b=3
-c=3.5
-
-result1 = func1(a,b)
-result2 = func2(a,b,c)
-print("Result1 = ", result1)
-print("Result2 = ", result2)
-
---math test
-local a,b = 2,4; a = a + 4 * b - a / 2 ^ b % 3
-print("a + 4 * b - a / 2 ^ b % 3 = ", a)
-
---conditional jumps test
-if a ~= b 
-	then print("a != b")
-end
-if a > b 
-	then print("a > b")
-end
-if a >= b 
-	then print("a >= b")
-end
-if a < b 
-	then print("a < b")
-end
-if a <= b 
-	then print("a <= b")
-end
-if a == b 
-	then print("a == b")
+a = 15
+while a - 1 > 0 do
+ 	print(a)
+ 	a = a - 1
 end
